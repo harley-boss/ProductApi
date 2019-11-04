03514c045ca643d2a1ccdc33923d87b65fde4f07
haboss <harleyaboss@gmail.com>
2019-11-04 08:51:47 -0500

    Initial commit of ProductApi

---
+++ ProductApi/DataAccess/ProductContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApi.Models;
+
+
+namespace ProductApi.DataAccess {
+
+    public class ProductContext : DbContext {
+
+        public ProductContext(DbContextOptions<ProductContext> options)
+            : base(options) {
+
+        }
+
+
+        public ProductContext() {
+
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
+
---
+++ ProductApi/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductApi.Models {
+    public class Product {
+
+        [Key]
+        public int prodId { get; set; }
+
+        public string prodName { get; set; }
+
+        public double price { get; set; }
+
+        public double prodWeight { get; set; }
+
+        public string inStock { get; set; }
+    }
+}
